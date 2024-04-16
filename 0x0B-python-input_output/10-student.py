f78e5dd2d7efb03491964ecf25ab7801ebd3cd27
gichobih <dennismuri254@gmail.com>
2024-04-16 10:30:10 -0700

    10-student.py

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+"""
+Module: 10-student
+"""
+
+
+class Student:
+    """
+    Class that defines a student.
+    """
+
+    def __init__(self, first_name, last_name, age):
+        """
+        Initializes a Student instance with first name, last name, and age.
+
+        Args:
+            first_name (str): The first name of the student.
+            last_name (str): The last name of the student.
+            age (int): The age of the student.
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """
+        Retrieves a dictionary representation of a Student instance.
+
+        Args:
+            attrs (list): A list of strings specifying attribute names to be retrieved.
+                          If None, all attributes will be retrieved.
+
+        Returns:
+            dict: A dictionary representation of the Student instance.
+        """
+        if attrs is None:
+            return self.__dict__
+        else:
+            return {attr: getattr(self, attr) for attr in attrs if hasattr(self, attr)}
+
