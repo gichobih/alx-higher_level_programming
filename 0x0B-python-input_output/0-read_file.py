29f34b2d8d755f17ba371a09310d63c3be6fcfe0
gichobih <dennismuri254@gmail.com>
2024-04-16 09:24:59 -0700

    0-read_file.py

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,5 +1,23 @@
 #!/usr/bin/python3
+"""
+Module: 0-read_file
+"""
+
+
 def read_file(filename=""):
-    with open(filename, encoding="utf-8") as fd:
-        for line in fd:
-            print(line, end="")
+    """
+    Read the content of a text file and print it to stdout.
+
+    Args:
+        filename (str): The name of the file to read. Defaults to "".
+
+    Returns:
+        None
+    """
+    with open(filename, 'r', encoding='utf-8') as file:
+        print(file.read(), end='')
+
+
+if __name__ == "__main__":
+    read_file("my_file_0.txt")
+
