6281f87416211663bae2b3906ebb86273aa4f332
gichobih <dennismuri254@gmail.com>
2024-04-17 15:18:03 -0700

    2-is_same_class.py

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,3 +1,14 @@
 #!/usr/bin/python3
+
 def lookup(obj):
+    """
+    Returns the list of available attributes and methods of an object.
+
+    Args:
+        obj (object): The object to inspect.
+
+    Returns:
+        list: A list of strings representing the attributes and methods of the object.
+    """
     return dir(obj)
+
