d2e785c0c1512314c92ef8dce18ec7d4dcac9bdd
gichobih <dennismuri@gmail.com>
2024-07-04 14:42:07 -0700

    102-catch_me.sh

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-# script to make a request to 0.0.0.0:5000/catch_me
+# Script to make a request to 0.0.0.0:5000/catch_me
 
-curl -s -L -X PUT -d "user_id=98" 0.0.0.0:5000/catch_me
+curl -s -L -X PUT 0.0.0.0:5000/catch_me -d "user_id=98" -H "Origin: School"
+
 
