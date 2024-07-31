72aee8f0b28e25c092def844442b2c2b9cacb37f
gichobih <dennismuri254@gmail.com>
2024-07-31 02:17:19 -0700

    101-starwars_characters.js

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -7,32 +7,19 @@ const url = `https://swapi-api.alx-tools.com/api/films/${id}`;
 request.get(url, (error, response, body) => {
   if (error) {
     console.log(error);
-    return;
+  } else {
+    const content = JSON.parse(body);
+    const characters = content.characters;
+    // console.log(characters);
+    for (const character of characters) {
+      request.get(character, (error, response, body) => {
+        if (error) {
+          console.log(error);
+        } else {
+          const names = JSON.parse(body);
+          console.log(names.name);
+        }
+      });
+    }
   }
-
-  if (response.statusCode !== 200) {
-    console.log(`Failed to retrieve film data. Status code: ${response.statusCode}`);
-    return;
-  }
-
-  const content = JSON.parse(body);
-  const characters = content.characters;
-
-  characters.forEach((character) => {
-    request.get(character, (error, response, body) => {
-      if (error) {
-        console.log(error);
-        return;
-      }
-
-      if (response.statusCode !== 200) {
-        console.log(`Failed to retrieve character data. Status code: ${response.statusCode}`);
-        return;
-      }
-
-      const characterData = JSON.parse(body);
-      console.log(characterData.name);
-    });
-  });
 });
-
