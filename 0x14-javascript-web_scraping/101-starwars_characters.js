1bd268d5ebe326174e33611965524f8c0f7b6adb
gichobih <dennismuri254@gmail.com>
2024-07-31 02:15:03 -0700

    101-starwars_characters.js

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -6,24 +6,33 @@ const url = `https://swapi-api.alx-tools.com/api/films/${id}`;
 
 request.get(url, (error, response, body) => {
   if (error) {
-    console.error('Error fetching film data:', error);
-  } else if (response.statusCode === 200) {
-    const content = JSON.parse(body);
-    const characters = content.characters;
-    characters.forEach((character) => {
-      request.get(character, (error, response, body) => {
-        if (error) {
-          console.error('Error fetching character data:', error);
-        } else if (response.statusCode === 200) {
-          const characterData = JSON.parse(body);
-          console.log(characterData.name);
-        } else {
-          console.error(`Failed to retrieve character data. Status code: ${response.statusCode}`);
-        }
-      });
-    });
-  } else {
-    console.error(`Failed to retrieve film data. Status code: ${response.statusCode}`);
+    console.log(error);
+    return;
   }
+
+  if (response.statusCode !== 200) {
+    console.log(`Failed to retrieve film data. Status code: ${response.statusCode}`);
+    return;
+  }
+
+  const content = JSON.parse(body);
+  const characters = content.characters;
+
+  characters.forEach((character) => {
+    request.get(character, (error, response, body) => {
+      if (error) {
+        console.log(error);
+        return;
+      }
+
+      if (response.statusCode !== 200) {
+        console.log(`Failed to retrieve character data. Status code: ${response.statusCode}`);
+        return;
+      }
+
+      const characterData = JSON.parse(body);
+      console.log(characterData.name);
+    });
+  });
 });
 
