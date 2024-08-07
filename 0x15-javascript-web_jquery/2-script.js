3655d9a6e7828562bbe5fbff23bea89b054fe8ab
gichobih <dennismuri254@gmail.com>
2024-08-07 03:43:44 -0700

    0x15-javascript-web_jquery

---
+++ 0x15-javascript-web_jquery/2-script.js
@@ -0,0 +1,6 @@
+//JavaScript script that updates the text color of the <header> element, 
+//To red (#FF0000) when the user clicks on the tag DIV#red_header
+
+$('div#red_header').click(function (){
+$('header').css('color', '#FF0000');
+});
